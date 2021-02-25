5ca8e6ad1318fb766ca60087d2b475ba92a70486
TatsuroTozawa <tgty0414@gmail.com>
2021-02-25 10:08:39 +0900

    Fix_20210225

--- js/script.js
+++ js/script.js
@@ -1,3 +1,5 @@
+'use strict'
+
 $(function() {
 
     $('#back a').on('click',function() {
@@ -14,3 +16,27 @@ $(function() {
 		$('.menu , .btn-line').toggleClass('open');
 	})
 });
+
+let todo = ['<i class="fab fa-html5"> HTML5', '<i class="fab fa-css3-alt"> CSS', '<i class="fab fa-js"> JavaScript'];
+for(let item of todo) {
+	const li = `<li>${item}</li>`;
+	document.getElementById('front-end').insertAdjacentHTML('beforeend', li);
+}
+
+let todo1 = ['jQuery', 'Ruby on Rails'];
+for(let item of todo1) {
+	const li = `<li>${item}</li>`;
+	document.getElementById('framework').insertAdjacentHTML('beforeend', li);
+}
+
+let todo2 = ['Ruby'];
+for(let item of todo2) {
+	const li = `<li>${item}</li>`;
+	document.getElementById('backend').insertAdjacentHTML('beforeend', li);
+}
+
+let todo3 = ['<i class="fab fa-github"> Git/GitHub'];
+for(let item of todo3) {
+	const li = `<li>${item}</li>`;
+	document.getElementById('devops').insertAdjacentHTML('beforeend', li);
+}
