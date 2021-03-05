962ca1622ab1e73077e95c061731dbb9271822a0
TatsuroTozawa <tgty0414@gmail.com>
2021-03-05 23:35:21 +0900

    fix_20210305

--- js/script.js
+++ js/script.js
@@ -1,6 +1,5 @@
 'use strict'
 
-// TOPへ戻るボタン
 $(function() {
     $('#back a').on('click',function() {
         $('body, html').animate( {
@@ -11,39 +10,33 @@ $(function() {
 
 });
 
-// ハンバーガーメニュー
 $(function() {
 	$('.js-btn').on('click',function() {
 		$('.menu , .btn-line').toggleClass('open');
 	})
 });
 
-// skill
 let todo = ['<i class="fab fa-html5"> HTML5', '<i class="fab fa-css3-alt"> CSS', '<i class="fab fa-js"> JavaScript'];
 for(let item of todo) {
 	const li = `<li>${item}</li>`;
 	document.getElementById('front-end').insertAdjacentHTML('beforeend', li);
 }
-
 let todo1 = ['jQuery', 'Ruby on Rails'];
 for(let item of todo1) {
 	const li = `<li>${item}</li>`;
 	document.getElementById('framework').insertAdjacentHTML('beforeend', li);
 }
-
 let todo2 = ['Ruby'];
 for(let item of todo2) {
 	const li = `<li>${item}</li>`;
 	document.getElementById('backend').insertAdjacentHTML('beforeend', li);
 }
-
 let todo3 = ['<i class="fab fa-github"> Git/GitHub'];
 for(let item of todo3) {
 	const li = `<li>${item}</li>`;
 	document.getElementById('devops').insertAdjacentHTML('beforeend', li);
 }
 
-// ナビゲーションメニュー
 $(document).ready(function(){
 	$('.submenu h3').on('click', function(){
 		$(this).next().toggleClass('hidden');
