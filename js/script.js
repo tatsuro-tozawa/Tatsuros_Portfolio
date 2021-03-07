d9b5c3717835c2cd14d6bb11fefbac66d3f31d38
TatsuroTozawa <tgty0414@gmail.com>
2021-03-07 18:51:52 +0900

    Fix_20210307

--- js/script.js
+++ js/script.js
@@ -19,22 +19,22 @@ $(function() {
 let todo = ['<i class="fab fa-html5"> HTML5', '<i class="fab fa-css3-alt"> CSS', '<i class="fab fa-js"> JavaScript'];
 for(let item of todo) {
 	const li = `<li>${item}</li>`;
-	document.getElementById('front-end').insertAdjacentHTML('beforeend', li);
+	document.getElementById('js-frontend').insertAdjacentHTML('beforeend', li);
 }
-let todo1 = ['jQuery', 'Ruby on Rails'];
+let todo1 = ['jQuery', 'Ruby on Rails', 'Bootstrap'];
 for(let item of todo1) {
 	const li = `<li>${item}</li>`;
-	document.getElementById('framework').insertAdjacentHTML('beforeend', li);
+	document.getElementById('js-framework').insertAdjacentHTML('beforeend', li);
 }
 let todo2 = ['Ruby'];
 for(let item of todo2) {
 	const li = `<li>${item}</li>`;
-	document.getElementById('backend').insertAdjacentHTML('beforeend', li);
+	document.getElementById('js-backend').insertAdjacentHTML('beforeend', li);
 }
-let todo3 = ['<i class="fab fa-github"> Git/GitHub'];
+let todo3 = ['<i class="fab fa-github"> Git/GitHub', 'Vagrant/VirtualBox', 'Slack'];
 for(let item of todo3) {
 	const li = `<li>${item}</li>`;
-	document.getElementById('devops').insertAdjacentHTML('beforeend', li);
+	document.getElementById('js-devops').insertAdjacentHTML('beforeend', li);
 }
 
 $(document).ready(function(){
